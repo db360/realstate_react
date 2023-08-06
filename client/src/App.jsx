886a91483b92ff4d9d072d84a7326f9ed2d22a17
David Martinez <da.b@hotmail.es>
2023-08-06 17:00:28 +0200

    Companies comp, counters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/Companies/Companies";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
     </div>
   );
 }
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,3 +1,6 @@
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+
 import "./Hero.css";
 const Hero = () => {
   return (
@@ -6,19 +9,47 @@ const Hero = () => {
         {/* Left Side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
-            <div className="orange-circle"/>
+            <div className="orange-circle" />
             <h1>
               Discover <br /> Most Suitable <br /> Property
             </h1>
           </div>
           <div className="flexColStart hero-desc">
-            <span>Find a variety of properties that suit you very easily</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easily
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
-          <div className="search-bar">
-            Search Bar
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
           </div>
-
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={88000} end={90000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winnings</span>
+            </div>
+          </div>
         </div>
         {/* Right Side */}
         <div className="flexCenter hero-right">

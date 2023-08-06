a5383358c6a6c43f9b6f8155a21b7798d47fbbec
David Martinez <da.b@hotmail.es>
2023-08-06 17:49:13 +0200

    Carousel, companies logos

---
+++ client/src/components/Residencies/Residencies.jsx
@@ -0,0 +1,36 @@
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Residencies.css";
+import data from "../../utils/slider.json";
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
+        <Swiper>
+            {
+                data.map((card, i) => (
+                    <SwiperSlide key={i}>
+                        <div className="r-card">
+                            <img src={card.image} alt="home picture" />
+                            <span className="secondaryText r-price">
+                                <span style={{color: "orange"}}>€</span>
+                                <span>{card.price}</span>
+                            </span>
+                            <span className="primaryText">{card.name}</span>
+                            <span className="secondaryText">{card.detail}</span>
+                        </div>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;

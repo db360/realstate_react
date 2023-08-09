83302e42b0c27c1b566b4c08680e367b55253298
David Martinez <da.b@hotmail.es>
2023-08-09 19:25:55 +0200

    value, contact, getstarted sections

---
+++ client/src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,21 @@
+import './GetStarted.css'
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className="primaryText">Get Started with Homyz</span>
+                <span className="secondaryText">Subscribe and find super attractive price quotes from us
+                    <br />
+                    Find Your residence soon
+                </span>
+                <button className='button'>
+                    <a href="mailto:da.b@hotmail.es">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted

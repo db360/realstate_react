eec8ca5a1049f6688494ce5ff35ba78a2d450342
David Martinez <da.b@hotmail.es>
2023-08-15 13:00:16 +0200

    mainpage animations

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,9 @@
+import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from 'framer-motion';
 
-import "./Hero.css";
+
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -10,9 +12,19 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
-              Discover <br /> Most Suitable <br /> Property
-            </h1>
+            <motion.h1
+              initial={ {y: "2rem", opacity: 0}}
+              animate={{y: 0, opacity: 1}}
+              transition={{
+                duration: 2,
+                type: "spring"
+
+              }}
+            >
+              Discover <br />
+              Most Suitable <br />
+              Property
+            </motion.h1>
           </div>
           <div className="flexColStart hero-desc">
             <span className="secondaryText">
@@ -22,11 +34,13 @@ const Hero = () => {
               Forget all difficulties in finding a residence for you
             </span>
           </div>
+
           <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
           </div>
+
           <div className="flexCenter stats">
             <div className="flexColCenter stat">
               <span>
@@ -53,9 +67,17 @@ const Hero = () => {
         </div>
         {/* Right Side */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0}}
+            animate={{ x: 0, opacity: 1}}
+            transition={{ 
+              duration: 2,
+              type: "spring"
+            }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>

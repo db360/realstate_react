075eab33f404ec41b99b268b59c39ebe9c9df499
David Martinez <da.b@hotmail.es>
2023-08-18 10:28:05 +0200

    new routes, useProperties hook, new structure

---
+++ client/src/pages/Property/Property.jsx
@@ -0,0 +1,86 @@
+import { useLocation } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getProperty } from "../../utils/api";
+import { PuffLoader } from "react-spinners";
+import { AiFillHeart, AiTwotoneCar } from "react-icons/ai";
+import { MdMeetingRoom } from "react-icons/md";
+import { FaShower } from "react-icons/fa";
+
+import "./Property.css";
+
+const Property = () => {
+  const { pathname } = useLocation();
+  const id = pathname.split("/")[2];
+  // console.log(id)
+  const { data, isLoading, isError } = useQuery(["resd", id], () =>
+    getProperty(id)
+  );
+
+  if (isLoading) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <PuffLoader />
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <span>Error While Fetching the property details</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="flexColStart paddings innerWidth property-container">
+        {/* like button */}
+        <div className="like">
+          <AiFillHeart size={24} color="white" />
+        </div>
+
+        {/* image */}
+        <img src={data?.image} alt="home image" />
+
+        <div className="flexCenter property-details">
+          {/* left side */}
+          <div className="flexColStart left">
+            {/* head */}
+            <div className="flexStart head">
+              <span className="primaryText">{data?.title}</span>
+              <span className="orangeText" style={{ fontSize: "1.5rem" }}>
+                € {data?.price}
+              </span>
+            </div>
+
+            {/* facilities */}
+            <div className="flexStart facilities">
+              <div className="flexStart facility">
+                <FaShower size={20} color="#1F3E72" />
+                <span>{data?.facilities?.bathrooms} Bathrooms</span>
+              </div>
+              <div className="flexStart facility">
+                <AiTwotoneCar size={20} color="#1F3E72" />
+                <span>{data?.facilities?.parkings} Parkings</span>
+              </div>
+              <div className="flexStart facility">
+                <MdMeetingRoom size={20} color="#1F3E72" />
+                <span>{data?.facilities?.bedrooms} Bedrooms</span>
+              </div>
+            </div>
+          </div>
+
+          {/* right side */}
+          <div className="right">this is right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Property;
